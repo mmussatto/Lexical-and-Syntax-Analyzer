fbb9d1595470b843f82e84e9baffded25f984149
mmussatto <murilomussatto@gmail.com>
2022-05-30 23:27:40 -0300

    Adding header information to source files

--- main.c
+++ main.c
@@ -1,3 +1,20 @@
+/*
+ * **************************************************
+ *                                                  *
+ * SSC 0605 - Teoria da Computação e Compiladores   *
+ *                                                  *
+ * Autores:                                         *
+ *  - Che Fan Pan                       - 11200421  *
+ *  - Eduardo Cavalari Valença          - 11234381  *
+ *  - Marcos Vinicius Firmino Pietrucci - 10770072  *
+ *  - Murilo Mussatto                   - 11234245  *
+ *                                                  *
+ * São Carlos - SP                                  *
+ * 2022                                             *
+ *                                                  *
+ * **************************************************
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "Source/file.h"
