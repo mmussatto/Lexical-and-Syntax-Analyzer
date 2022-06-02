c6540102c9f3063a07888e1d8a6d4e50d4664e80
mmussatto <murilomussatto@gmail.com>
2022-06-01 22:27:56 -0300

    Creating Headers Folder

---
+++ Headers/error.h
@@ -0,0 +1,34 @@
+/*
+ * **************************************************
+ *                                                  *
+ * SSC 0605 - Teoria da Computação e Compiladores   *
+ *                                                  *
+ * Autores:                                         *
+ *  - Che Fan Pan                       - 11200421  *
+ *  - Eduardo Cavalari Valença          - 11234381  *
+ *  - Marcos Vinicius Firmino Pietrucci - 10770072  *
+ *  - Murilo Mussatto                   - 11234245  *
+ *                                                  *
+ * São Carlos - SP                                  *
+ * 2022                                             *
+ *                                                  *
+ * **************************************************
+ */
+
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct 
+{
+    char* error_type;
+} error;
+
+
+error* create_error_vector(int NUM_ERRORS);
+
+void add_errors(error* vec_error);
+
+void free_error_vec(error* vec_error);
+
