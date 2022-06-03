3f4c557e62b4fa2f4c5ddcf5c643e75a914bfa23
EduardoCavValenca <eduardo.cvalenca@gmail.com>
2022-06-02 22:44:19 -0300

    Token files and settings ^-^

---
+++ Headers/token.h
@@ -0,0 +1,58 @@
+/*
+ * **************************************************
+ *                                                  *
+ * SSC 0605 - Teoria da Computação e Compiladores   *
+ *                                                  *
+ * Autores:                                         *
+ *  - Che Fan Pan                       - 11200421  *
+ *  - Eduardo Cavalari Valença          - 11234381  *
+ *  - Marcos Vinicius Firmino Pietrucci - 10770072  *
+ *  - Murilo Mussatto                   - 11234245  *
+ *                                                  *
+ * São Carlos - SP                                  *
+ * 2022                                             *
+ *                                                  *
+ * **************************************************
+ */
+
+#pragma once
+
+ //Store the possible tokens of the program
+typedef struct
+{
+    char* name; 
+    char* type;
+} token;
+
+typedef struct 
+{
+    token* tokens;
+    int size;
+} vec_token;
+
+/**
+ * @brief Create a tokens vector object
+ * 
+ * @return vec_token* 
+ */
+vec_token* create_tokens_vector();
+
+/**
+ * @brief 
+ * 
+ * @param vec_tokens 
+ * @return token 
+ */
+token last_vec_token(vec_token* vec_tokens);
+
+
+/**
+ * @brief 
+ * 
+ * @param vec_tokens 
+ * @param t 
+ */
+void vec_tokens_push_back(vec_token* vec_tokens, token t);
+
+
+void free_tokens_vector(vec_token* vec_tokens);
