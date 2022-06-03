88c09590c6d2c93e8baf351b05cb6c82e5e59456
EduardoCavValenca <eduardo.cvalenca@gmail.com>
2022-06-02 22:40:59 -0300

    Updating main FINAL FORM

--- main.c
+++ main.c
@@ -21,15 +21,15 @@
 
 int main(int argc, char** argv)
 {
-    FILE *program = read_new_file(argv[1],'r');
-
     //Checking runtime parameters
     if(argc != 2)
     {
         printf("Provide just the name of the file\n");
         return 1;
     }
-    
+      
+    FILE *program = read_new_file(argv[1],'r');
+  
     //Checking for error on file opening
     if(program == NULL)
     {
@@ -39,27 +39,33 @@ int main(int argc, char** argv)
     
     /*------ Lexical analyser logic --------*/
     int **transition_matrix = create_transition_matrix();
-    populate_transition_matrix(transition_matrix);
+    populate_transition_matrix(transition_matrix); // Matrix of transitions
 
-    state* final_states = create_states_vector(ROWS);
-    populate_states_vector(final_states);
+    state* vec_states = create_states_vector(NUM_STATES);
+    populate_states_vector(vec_states); // Vector of states
 
-    error *vec_error = create_error_vector(NUM_ERRORS);
-    populate_error_vector(vec_error);
+    error *vec_errors = create_errors_vector(NUM_ERRORS);
+    populate_errors_vector(vec_errors); // Vector of errors
 
-    token t;
+    vec_token *vec_tokens = create_tokens_vector();
 
-    while(strcmp(t.name,"EOF") != 0) //???
+    do
     {
-        t = get_token(program, transition_matrix, final_states);
-        //LEMBRAR DE DESALOCAR O TOKEN, A STRING DELE TA SENDO ALOCADA
-    }
+        vec_tokens_push_back(vec_tokens, get_token(program, transition_matrix, vec_states));
+       
+    } while(strcmp(last_vec_token(vec_tokens).name,"EOF") != 0); 
+
+    FILE *f_out = create_new_file("saida.txt");
+
+    write_tokens_file(f_out, vec_tokens);
 
     //free data structures
     free_transition_matrix(transition_matrix);
-    free_states_vector(final_states);
-    free_error_vector(vec_error);
+    free_states_vector(vec_states);
+    free_errors_vector(vec_errors);
+    free_tokens_vector(vec_tokens);
     
     fclose(program);
+    fclose(f_out);
     return 0;
 }
