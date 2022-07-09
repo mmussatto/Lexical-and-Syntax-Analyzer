471239fd2b3fccde6674df0556620cc6b0ce6b07
mmussatto <murilomussatto@gmail.com>
2022-07-09 12:52:00 -0300

    Adding new get token function

--- Source/syntax.c
+++ Source/syntax.c
@@ -16,3 +16,14 @@
  */
 
 #include "Headers/syntax.h"
+token* get_token_from_vector(vec_token* vec_tokens, token *curr_token)
+{   
+    vec_tokens->n_curr_token++;
+
+    curr_token->name = strdup(vec_tokens->tokens[vec_tokens->n_curr_token].name);
+    curr_token->type = strdup(vec_tokens->tokens[vec_tokens->n_curr_token].type);
+    curr_token->line = vec_tokens->tokens[vec_tokens->n_curr_token].line;
+
+    //curr_token->name = vec_tokens->tokens[vec_tokens->n_curr_token].name;
+    return curr_token;
+}
